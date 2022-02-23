a5e66288538324d963996ce522564f85eee64d0c
cexposit <cexposit@ull.edu.es>
2022-02-23 10:29:28 +0000

    reverse

--- practice/reverse-engineering/reverse.c
+++ practice/reverse-engineering/reverse.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 int main(int argc, char **argv) {
+  if (argc != 2) {
+    printf("Incorrect usage. Try: ./EXECUTABLE PASSWORD\n");
+    exit (EXIT_FAILURE);
+  }
   char flag[] = "verysecretpassword";
-  if(strcmp(argv[1], flag) == 0) {
+  if (strcmp(argv[1], flag) == 0) {
     printf("Correct password!\n");
   } else {
-    printf("Incorrect password. Try again\n");
+    printf("Incorrect password!\n");
   }
   return 0;
 }
